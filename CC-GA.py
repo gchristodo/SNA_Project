2146db9e834459bc4f019633a35c9d753518507b
george sidiras <georgesidiras@gmail.com>
2020-01-11 14:03:31 +0200

    Graph with weigts// add the wigts from each eidge from the gross category 1 if the gross is less 100000)/ category 2 gross is between 400.000 and 1.000.000  3 gross isbetween 1.000.000 and 10.000.000   4 gross is between 10.000.000 and 100.000.000   5 gross is between 100.000.000 and 1.000.000.000   6 gross between 1.000.000.000 and 1.000.000.000

--- CC-GA.py
+++ CC-GA.py
@@ -5,25 +5,49 @@ df = pd.read_csv('movies.csv')
 #delete null values
 df=df[df['director_name'].notnull()]
 
+#Append to dataframe evaloution from gross of movie
+evalo=[]
+for row in df['gross']:
+    if (row <100000) :evalo.append(1)
+    elif (row >100000 and row <400000) :evalo.append(2)
+    elif (row >400000 and row <1000000) :evalo.append(3)
+    elif (row >1000000 and row <10000000) :evalo.append(4)
+    elif (row >10000000 and row <50000000) :evalo.append(5)
+    elif (row >50000000 and row <100000000) :evalo.append(6)
+    elif (row >100000000 and row <200000000) :evalo.append(7)
+    elif (row >200000000 and row <400000000) :evalo.append(8)
+    elif (row >400000000):evalo.append(9)
+    else:evalo.append(0)
+df["eval"]=evalo
+
 #Take the actor1 af dataframe1 and dataframe2 actor2,Alseo 4
 
 
-df1 = df[['director_name','actor_1_name']]
-df2=df[['director_name','actor_2_name']]
+df1 = df[['director_name','actor_1_name','gross','eval']]
+df2=df[['director_name','actor_2_name','gross','eval']]
 
 #rename coloumn
-df2.columns = ['director_name', 'actor_1_name']
+df2.columns = ['director_name', 'actor_1_name','gross','eval']
 
 #merge df2 and df2
 
 
 dff= df1.append(df2)
-df3 = df[['director_name','actor_3_name']]
+df3 = df[['director_name','actor_3_name','gross','eval']]
+
+
 #rename coloumn
-df3.columns = ['director_name', 'actor_1_name']
+df3.columns = ['director_name', 'actor_1_name','gross','eval']
 fdf= dff.append(df3)
 
-fdf.columns = ['director_name', 'actor_name']
+fdf.columns = ['director_name', 'actor_name','gross,','eval']
+
+graphData = fdf[['director_name','actor_name','eval']]
+
+
+#Below we sum the actor each coloboration
+fdfcnta=fdf.groupby('director_name')['actor_name'].count()
+fdfcnta=['director_name', 'numA']
 
 #reseting index
 #fdf = fdf.reset_index()
@@ -34,6 +58,7 @@ fdf.to_csv('Direc_actor_data.csv',index=False)
 
 #create the graph
 g = nx.from_pandas_edgelist(fdf, 'director_name', 'actor_name')
+G = nx.from_pandas_edgelist(graphData, 'director_name', 'actor_name','eval')
             
             #returns the noubors list
 def neib(ne):
